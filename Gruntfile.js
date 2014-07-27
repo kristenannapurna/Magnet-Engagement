18ed79a6fe932b534d715fe402b92fd3779d4bfc
kristencodes <kristenamspencer@gmail.com>
2014-07-27 19:18:35 -0400

    Made Readme file sound like actual english

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,7 +6,7 @@ module.exports = function(grunt) {
    sass: {
    	dist: {
    		files: {
-   		'Dist/style.css' : 'dev/sass/style.scss'
+   		'dist/style.css' : 'dev/sass/style.scss'
    		}
    	} 	
    }, 
@@ -24,7 +24,7 @@ module.exports = function(grunt) {
    		browsers: ['last 5 version', 'ie 7', 'ie 8', 'ie 9']
    	}, 
    	no_dest: {
-   		src: 'Dist/style.css'
+   		src: 'dist/style.css'
    	}
    },
    connect: {
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Magnet-Engagement
 =================
 
-This was a mockup multi-page web mockup I did for a marketing agency. 
+This was a mockup site I did for a marketing agency. 
